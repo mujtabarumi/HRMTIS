ba16f47a2a15aeb6e17a24798a220a67b2453850
rjait71@gmail.com <42765605+totalcable@users.noreply.github.com>
2019-10-21 14:33:32 +0600

    faruk

--- backend/app/Http/Controllers/DesignationController.php
+++ backend/app/Http/Controllers/DesignationController.php
@@ -29,6 +29,7 @@ class DesignationController extends Controller
             'salaryGrade' =>'nullable|max:10',
             'desigLevel' =>'nullable|max:4',
             'created_by' =>'nullable|max:20',
+            'status' =>'required|max:20',
         ]);
         if($r->id){
             $desigNationInfo = Designation::findOrFail($r->id);
@@ -40,6 +41,7 @@ class DesignationController extends Controller
         $desigNationInfo->shortName= $r->shortName;
         $desigNationInfo->salaryGrade = $r->salaryGrade;
         $desigNationInfo->desigLevel= $r->desigLevel;
+        $desigNationInfo->status= $r->status;
         $desigNationInfo->created_by = auth()->user()->id;
         $desigNationInfo->save();
         return response()->json(["message"=>"Designation Saved"]);
